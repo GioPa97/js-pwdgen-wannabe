f6646cd7ab07eae2aa26088db3f04945c3de68a9
GioPa97 <giovannipagliara97@gmail.com>
2022-11-08 16:29:19 +0100

    Bonus1 - concatenazione con dividendo

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let nome = prompt("Inserisci il tuo nome");
 let cognome = prompt("Inserisci il tuo cognome");
 let colore = prompt("Inserisci il tuo colore preferito");
+let numero1 = prompt("Inserisci un numero")
+let numero2 = prompt("Inserisci un altro numero")
+
 
 console.log(
     `
     Informazioni___________________________________________________
 
-        1)Nome:    ${nome}
-        2)Cognome: ${cognome}
-        3)Colore:  ${colore}
+        1)Nome:           ${nome}
+        2)Cognome:        ${cognome}
+        3)Colore:         ${colore}
+        4)Primo numero:   ${numero1}
+        5)Secondo numero: ${numero2}
 
-        Password:  ${nome + cognome + colore + 21}
+        Password:  ${nome + cognome + colore + (numero1 / numero2)}
 
     ____________________________________________________Informazioni
     `
